41c0fb355287f121bca24a3f178a56d33a46f413
yashkulkarni98 <53830486+yashkulkarni98@users.noreply.github.com>
2020-05-24 02:13:34 +0530

    Create Looping and Skipping.sh

---
+++ Linux Shell/Looping and Skipping.sh
@@ -0,0 +1,46 @@
+Your task is to use for loops to display only odd natural numbers from  to .
+
+Input Format
+
+There is no input.
+
+Constraints
+
+-
+
+Output Format
+
+1
+3
+5
+.
+.
+.
+.
+.
+99  
+Sample Input
+
+-
+
+Sample Output
+
+1
+3
+5
+.
+.
+.
+.
+.
+99  
+Explanation
+
+-
+
+ans 
+
+for i in {1..100..2}
+  do
+     echo  $i 
+ done
