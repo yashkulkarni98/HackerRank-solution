ffe4cd8f80edb293269afddd406382adf9168fa1
yashkulkarni98 <53830486+yashkulkarni98@users.noreply.github.com>
2020-05-24 23:21:29 +0530

    Update Day 1: Interquartile Range.py

--- 10 days of statistics/Day 1: Interquartile Range.py
+++ 10 days of statistics/Day 1: Interquartile Range.py
@@ -1,31 +1,31 @@
-#!/bin/python3
-import statistics
+from __future__ import division
 
-L = list()
-U = list()
-S = list()
+def get_median(a):
+    #print a
+    n = len(a)
+    median = a[n//2]
+    #print median
+    if n%2 == 0:
+        #print a[n/2-1:n/2]
+        median = sum(a[n//2-1:n//2+1])/2
+    return median
+        
+n = int(raw_input())
+a = map(int, raw_input().split())
+freq = map(int, raw_input().split())
+array = []
+for i in range(n):
+    array.extend([a[i] for x in range(freq[i])])
+array.sort()
+n = len(array)
+#print array, n
+median = get_median(array)
+l = [x for x in array[0:n//2] if x < median]
+r = [x for x in array[n//2+1:n] if x > median]
+if n % 2 == 0:
+    l = [x for x in array[0:n//2] if x < median]
+    r = [x for x in array[n//2:n] if x > median]
 
-N = int(input().strip())
-X = [int(e) for e in input().split(' ')]
-F = [int(e) for e in input().split(' ')]
-
-def freq_append(val, freq):
-    for x in range(freq):
-        S.append(val)
-
-for i in range(N):
-    freq_append(X[i], F[i])
-
-median = statistics.median(S)
-
-for x in S:
-    if x < median:
-        L.append(x)
-    elif x > median:
-        U.append(x)
-
-Q1 = statistics.median(L)
-Q2 = median
-Q3 = statistics.median(U)
-
-print(Q3 - Q1)
+#print l, get_median(l)
+#print r, get_median(r)
+print round(get_median(r) - get_median(l), 1)
